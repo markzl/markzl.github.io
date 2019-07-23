f0c2d23eddcd1a34b7e1503ddc588e3ca9eea0c0
markzl <zhoulong2312@163.com>
2019-07-23 10:54:05 +0800

    Site updated: 2019-07-23 10:54:05

--- js/gallery.js
+++ js/gallery.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
         $('.article').lightGallery({ selector: '.gallery-item' });
     }
     if (typeof ($.fn.justifiedGallery) === 'function') {
-        $('.justified-gallery').justifiedGallery();
+        // $('.justified-gallery').justifiedGallery();
+        $('.justified-gallery').justifiedGallery({rowHeight:160, margins:4});
     }
 });
